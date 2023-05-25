ccd3c0e1aca4aa8f5b110a20830c80340876e600
tomaso <admin@kash.or.ke>
2023-05-25 17:27:24 +0300

    mysecond commit

--- app/src/main/java/com/example/tichlearngit/MainActivity.java
+++ app/src/main/java/com/example/tichlearngit/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.tichlearngit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+public TextView mytext;
+    public String myteststring;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mytext=findViewById(R.id.firstitem);
+        myteststring=mytext.toString();
     }
+
 }
