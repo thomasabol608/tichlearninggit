c2d5b8e1bad6ff7f037277f371ea84f39c59c760
tomaso <admin@kash.or.ke>
2023-05-25 17:32:16 +0300

    added color and text commit

--- app/src/main/java/com/example/tichlearngit/MainActivity.java
+++ app/src/main/java/com/example/tichlearngit/MainActivity.java
@@ -2,18 +2,23 @@ package com.example.tichlearngit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 public TextView mytext;
     public String myteststring;
+    public String thetext="Whats going on here....";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mytext=findViewById(R.id.firstitem);
+        mytext.setTextColor(Color.RED);
+        mytext.setText(thetext);
         myteststring=mytext.toString();
+
     }
 
 }
